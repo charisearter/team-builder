a45c27b03026b31bceff8ceb33737d3f9817886a
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-19 23:46:26 -0500

    fixing forms i hope i am still in my branch it does not say

--- README.md
+++ README.md
@@ -40,13 +40,13 @@ Follow these steps for completing your project.
 
 ### STEP 1 - Setup your state
 
-- [ ] Import the `useState` hook and set up state to keep your team members list.
+- [x] Import the `useState` hook and set up state to keep your team members list.
 - [ ] Give the state variable you just declared a default value. You will need to keep track of a list of team members and each team member will have several key/value pairs associated with them.
 - [ ] Render your list of team members.
 
 ### STEP 2 - Build your form
 
-- [ ] In `Form.js` build out your markup.
+- [x] In `Form.js` build out your markup.
 - [ ] Build inputs for `name`, `email` and `role` (backend engineer, frontend engineer, designer, etc. Use your imagination).
 - [ ] You will have to decide which component is responsible for maintaining the _state_ of the form (`Form` itself, or its parent `App`). Each approach has advantages and disadvantages.
 - [ ] Render your `Form` component in `App`. The `App` component should hand down through props any callback(s) needed for `Form` to do its job (adding new members to your team members state on submit).
--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './Form'
+
+
+//Team List Object
+const initialTeamList = [
+  {
+    teammate: 'Reese',
+    email: 'reese@gmail.com',
+    role: 'Full Stack Web Dev'
+  },
+];
+
+//Give State default value
+const initialFormValues = {
+  teammate: '',
+  email: '',
+  role: '',
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [team, setTeam] = useState(initialTeamList);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const onInputChange = evt => {
+ 
+    const { teammate } = evt.target;
+    const { value } = evt.target;
+    setFormValues({ ...formValues, [teammate]: value })
+  };
+
+  const onSubmit = evt => {
+
+    evt.preventDefault();
+   
+    if (
+      !formValues.teammate.trim() ||
+      !formValues.email.trim() ||
+      !formValues.role.trim()
+    ) {
+      return;
+    }
+  
+    return (
+      <div className="App">
+        <h1>Team List</h1>
+        <Form
+          values={formValues}
+          onInputChange={onInputChange}
+          onSubmit={onSubmit}
+        />
+      </div>
+    );
+  };
 }
 
 export default App;
